155b7249527268b56e516ffaf615f83493ffa430
user <user@user-PC>
2017-10-07 09:49:23 +0800

    Creating git project.

---
+++ com.splus.selenium/src/test/java/com/splus/selenium/SelDemo.java
@@ -0,0 +1,14 @@
+package com.splus.selenium;
+
+import org.testng.annotations.Test;
+
+public class SelDemo {
+
+	@Test
+	public void printSel()
+	{
+		
+		System.out.println("Sample Selenium script");
+	}
+	
+}
